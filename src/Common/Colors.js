e10a4a4c7d45afec4502c7f21629c7fc18e965ea
Mahendra Bhama <maahibhama@gmail.com>
2019-01-30 16:54:14 +0530

    Introduce color and font class

---
+++ src/Common/Colors.js
@@ -0,0 +1,21 @@
+export const Color = {
+    themeDark: '#000000',
+    themeLight: '#64B6FF',
+    themeExtraLight: '#E25C58',
+
+    themeBackground: '#FFFFFF',
+    offWhiteBackground: '#F6F6F6',
+
+    themeText: "#707070",
+    darkText: '#323232',
+    lightText: '#A6A6A6',
+    brightText: '#FFFFFF',
+    placeholderText: '#CCCCCC',
+
+    lightBorder: '#ff0000',
+    brightBorder: '#DADADA',
+    lightFill: '#BBBBBB',
+    shadow: '#000000',
+
+    actionTint: '#0072BB'
+}
